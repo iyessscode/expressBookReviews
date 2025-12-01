ec1f1a93e060c211826c95358286a5d678903eea
iyessscode <iyessscode@gmail.com>
2025-12-01 22:06:19 +0700

    Implement user login functionality with validation and JWT token generation

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -10,14 +10,37 @@ const isValid = (username) => {
 };
 
 const authenticatedUser = (username, password) => {
-	//returns boolean
-	//write code to check if username and password match the one we have in records.
+	return (
+		users.filter(
+			(user) => user.username === username && user.password === password
+		).length > 0
+	);
 };
 
 //only registered users can login
 regd_users.post('/login', (req, res) => {
-	//Write your code here
-	return res.status(300).json({ message: 'Yet to be implemented' });
+	const { username, password } = req.body;
+
+	if (!username || !password) {
+		return res
+			.status(400)
+			.json({ message: 'Username and password are required' });
+	}
+
+	if (!authenticatedUser(username, password)) {
+		return res.status(401).json({ message: 'Invalid username or password' });
+	}
+
+	let accessToken = jwt.sign({ data: password }, 'access', {
+		expiresIn: 60 * 60,
+	});
+
+	req.session.authorization = {
+		accessToken,
+		username,
+	};
+
+	return res.status(200).json({ message: 'User successfully logged in' });
 });
 
 // Add a book review
